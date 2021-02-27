ccaa21766b34572f7cb96fe532cad6805ecd8f66
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2021-02-26 21:57:03 -0500

    Parking lot activity
    -Added handler for on back pressed
    -Added alert icon for dialog box
    -Removed instances of hardcoded text

--- ParkX/src/main/java/com/parkx/ParkingLotActivity.java
+++ ParkX/src/main/java/com/parkx/ParkingLotActivity.java
@@ -1,5 +1,6 @@
 package com.parkx;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,22 @@ public class ParkingLotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lot);
+        setTitle(getString(R.string.parkinglot_title));
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.parkinglot_dialogbackmessage))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.parkinglot_dialogpositive), (dialog, which) -> {
+                    finishAffinity();
+                    System.exit(0);
+                })
+                .setNegativeButton(getString(R.string.parkinglot_dialognegative), (dialog, which) -> dialog.cancel());
+        AlertDialog dialog = builder.create();
+        dialog.setTitle(getString(R.string.parkinglot_dialogbacktitle));
+        dialog.setIcon(R.drawable.ic_alert);
+        dialog.show();
     }
 }
