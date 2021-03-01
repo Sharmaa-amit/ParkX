603a161f8552596af8aac54c99dec89601ffe1ac
Sharmaa-amit <71359389+Sharmaa-amit@users.noreply.github.com>
2021-03-01 14:55:12 -0500

    Parking lot activity
    -Added functionality for realtime database
    -Added handler for help button
    -Added function to load spots
    -Removed instances of hardcoded text

--- ParkX/src/main/java/com/parkx/ParkingLotActivity.java
+++ ParkX/src/main/java/com/parkx/ParkingLotActivity.java
@@ -1,17 +1,54 @@
 package com.parkx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ParkingLotActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    TextView tvSpots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lot);
         setTitle(getString(R.string.parkinglot_title));
+
+        database = FirebaseDatabase.getInstance();
+        tvSpots = findViewById(R.id.tvSpots);
+        loadSpots();
+
+        Button btnHelp = findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(v -> {
+            Toast.makeText(ParkingLotActivity.this, getString(R.string.parkinglot_helpmessage), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void loadSpots() {
+        DatabaseReference ref = database.getReference().child(getString(R.string.parkinglot_childspots));
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tvSpots.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 
     @Override
